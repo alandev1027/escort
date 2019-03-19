8738f5ffafae7bd0dc4654c664141b9e61d30881
Alan Brown <alandev1027@gmail.com>
2019-03-20 04:02:43 +0700

    escort site

---
+++ src/components/reference.js
@@ -0,0 +1,23 @@
+
+import * as firebase from "firebase";
+
+var config = {
+  apiKey: "AIzaSyDsTsClk8l2l5yLNu_eG-R06Usv4Oi_NvQ",
+  authDomain: "d-ucukgx.firebaseapp.com",
+  databaseURL: "https://d-ucukgx.firebaseio.com",
+  projectId: "d-ucukgx",
+  storageBucket: "d-ucukgx.appspot.com",
+  messagingSenderId: "1031835936266"
+};
+firebase.initializeApp(config);
+
+
+export const firedb = firebase.database();
+export const databaseRef = firebase.database().ref();
+export const ChampsRef = databaseRef.child("Champs");
+export const CommentsRef = databaseRef.child("Comments");
+export const CounterTipsRef = databaseRef.child("CounterTips");
+export const authRef = firebase.auth();
+export const timeRef = firebase.database.ServerValue.TIMESTAMP;
+
+export default databaseRef;
